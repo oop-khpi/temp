3ef21345f71cdfd8dc04c0bf31fe51d72d4bff02
xone <xone@ukr.net>
2021-02-19 13:29:11 +0200

    repo: fix bugs

--- ex02/src/ex02/Container.java
+++ ex02/src/ex02/Container.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 
 public class Container<E> {
 	private Object[] array = new Object[0];
+
 	void add(E data) {
 //		Object[] old = array;
 //		array = new Object[old.length + 1];
@@ -15,26 +16,46 @@ public class Container<E> {
 		array = Arrays.copyOf(array, array.length + 1);
 		array[array.length - 1] = data;
 	}
-	
-	public <T> T[] toArray() {
-            return (T[]) Arrays.copyOf(array, array.length);
-    }
-	
+
+	public E remove(int index) {
+		@SuppressWarnings("unchecked")
+		E removed = (E) array[index];
+		int i = array.length - 1;
+		if (index < i) {
+			System.arraycopy(array, index + 1, array, index, i - index);
+		}
+		array[i] = null;
+		return removed;
+	}
+
+	public Object[] toArray() {
+		return Arrays.copyOf(array, array.length);
+	}
+
+	public <T> T[] toArray(T[] a) {
+		if (a.length < array.length) {
+			@SuppressWarnings("unchecked")
+			T[] result = (T[]) Arrays.copyOf(array, array.length, a.getClass());
+			return result;
+		}
+		System.arraycopy(array, 0, a, 0, array.length);
+		return a;
+	}
+
 	@Override
 	public String toString() {
-		// return "Container:\n" + Arrays.toString(array);
 		StringBuilder buf = new StringBuilder();
 		for (Object object : array) {
 			buf.append(object).append(System.lineSeparator());
 		}
+		buf.delete(buf.length() - System.lineSeparator().length(), buf.length());
 		return buf.toString();
 	}
-	
+
 	void sort(Comparator<? super E> c) {
-        //Arrays.sort(a, (Comparator) c);
-        E[] a = this.toArray();
-        Arrays.sort(a, c);
-        array = a;
-    }
+		@SuppressWarnings("unchecked")
+		E[] a = (E[]) array;
+		Arrays.sort(a, c);
+	}
 
 }
--- ex02/src/ex02/Main.java
+++ ex02/src/ex02/Main.java
@@ -6,24 +6,24 @@ import java.util.Comparator;
 public class Main {
 
 	public static void main(String[] args) {
+		// See: ArrayList, Collections, Arrays, System.
 		System.out.println("Begin...");
 		Library library = new Library("1234567890", "no name", "no author", "a-ba-ba-ha-la-ma-ha", "pulp fiction",
 				"08/02/2021");
 		System.out.println(library);
 		System.out.println("---");
-		Library[] array = { new Library("1234567890", "name1", "author1", "publisher1", "genre1", "date1"),
+		Library[] array = { new Library("1234567891", "name1", "author1", "publisher1", "genre1", "date1"),
 				new Library("3282849494", "name2", "author2", "publisher2", "genre2", "date2"),
-				new Library("9494873772", "name3", "author3", "publisher3", "genre1", "date3") };
-		
+				new Library("9494873779", "name3", "author3", "publisher3", "genre1", "date3") };
+
 		System.out.println(Arrays.toString(array));
 		System.out.println("---");
-		// ArrayList HashSet AbstractCollection Collections
 		Container<Library> container = new Container<>();
 		container.add(library);
 		for (Library lib : array) {
 			container.add(lib);
 		}
-		System.out.print(container);
+		System.out.println(container);
 		System.out.println("---");
 		container.sort(new Comparator<Library>() {
 			public int compare(Library o1, Library o2) {
@@ -34,10 +34,18 @@ public class Main {
 		System.out.println("---");
 		sort(container, (o1, o2) -> o1.getName().compareTo(o2.getName()));
 		System.out.println(container);
+		System.out.println("---");
+		for (Library x : container.toArray(new Library[0])) {
+			System.out.println(x + " <- " + x.getIsbn().matches("^(9).*\\1$"));
+		}
+		System.out.println("---");
+		Library xxx = container.remove(3);
+		System.out.println(container);
+		System.out.println(xxx);
 		System.out.println("Done.");
 	}
 
-	public static <T> void sort(Container<T> list, Comparator<? super T> c) {
-        list.sort(c);
-    }
+	public static <T> void sort(Container<T> container, Comparator<? super T> c) {
+		container.sort(c);
+	}
 }
